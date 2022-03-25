b67321208907558e9515e3a8c0e9bd5be3c3b5b7
tubatasnim <tasnimtuba22@gmail.com>
2022-03-26 00:48:21 +0600

    pizza picturs add

---
+++ src/component/Pizza/Pizza.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pizza = () => {
+    return (
+        <div>
+            <h3>One pizza</h3>
+        </div>
+    );
+};
+
+export default Pizza;
---
+++ src/component/Pizzas/Pizzas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pizza from '../Pizza/Pizza';
+
+const Pizzas = () => {
+    return (
+        <div>
+            <h1>Pizza Hut</h1>
+            <Pizza></Pizza>
+        </div>
+    );
+};
+
+export default Pizzas;
